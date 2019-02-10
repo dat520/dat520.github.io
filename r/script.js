af15b5aca7544841dc94ed1cabcc579ff21c92d1
Racin Nygaard <racin.nygaard@gmail.com>
2019-02-10 10:42:40 +0100

    Add missing var

--- r/script.js
+++ r/script.js
@@ -1,7 +1,7 @@
 var baseUrl = "https://github.com/uis-dat520-s2019/"
 
 function Redirect() {
-    redirect = baseUrl + window.location.search.substr(1);
+    var redirect = baseUrl + window.location.search.substr(1);
 
     document.body.innerHTML += "<br /><a href='"+redirect+"'>"+redirect+"</a>";
     window.location.replace(redirect);
