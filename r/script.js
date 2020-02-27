0eb38e62a9497b7754b278d85e0bdaa4d530f9eb
Racin Nygaard <racin.nygaard@gmail.com>
2020-02-27 14:08:14 +0100

    Lab 2020

--- r/script.js
+++ r/script.js
@@ -1,8 +1,8 @@
-var baseUrl = "https://github.com/uis-dat520-s2019/"
+var baseUrl = "https://github.com/dat520-2020/"
 
 function Redirect() {
     var redirect = baseUrl + window.location.search.substr(1) + window.location.hash;
 
-    document.body.innerHTML += "<br /><a href='"+redirect+"'>"+redirect+"</a>";
+    document.body.innerHTML += "<br /><a href='" + redirect + "'>" + redirect + "</a>";
     window.location.replace(redirect);
 }
